c1f588f61f9f7f1a326894344da16d00ec6ca242
William <william@pixney.com>
2018-12-04 20:44:03 +0100

    second commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Appointments',
+    'name'        => 'Appointments Module',
+    'description' => ''
+];
---
+++ tests/Feature/AppointmentsModuleTest.php
@@ -0,0 +1,10 @@
+<?php namespace Pixney\AppointmentsModule\Test\Feature;
+
+class AppointmentsModuleTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
